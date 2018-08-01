8a1329c5c980e9c7e8b2878b61b93220be4a4ee1
Danilo Oliveira <danilo.oliveira@recife.pe.gov.br>
2018-08-01 19:35:10 -0300

    #000 - Ajuste para envio de e-mail

--- src/Kernel.php
+++ src/Kernel.php
@@ -5,11 +5,13 @@ namespace App;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\Config\Resource\FileResource;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
 
-class Kernel extends BaseKernel
+
+class Kernel extends BaseKernel implements CompilerPassInterface
 {
     use MicroKernelTrait;
 
@@ -73,5 +75,4 @@ class Kernel extends BaseKernel
             'fos_user.security.login_manager'
         ];
     }
-
 }
